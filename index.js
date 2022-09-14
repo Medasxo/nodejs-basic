a6461e21c64b74677e7b0b3d8b1be698698f8ffb
Medas <medas02345@gmail.com>
2022-09-14 21:48:43 +0300

    Add html contents to every page and linked them in the index.js so that :8080/about would open about.html and etc.

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-const aboutPage = require("./about.html");
 const http = require("http");
 const port = process.env.PORT || 8080;
 const fs = require("fs");
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
-  fs.readFile("index.html", "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    res.end(data);
-  });
-
+  if (req.url === "/") {
+    fs.readFile("index.html", "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.end(data);
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("about.html", "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.end(data);
+    });
+  }
+  else if(req.url === "/contact-me") {
+    fs.readFile("contact-me.html", "utf-8", (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        res.end(data);
+      });
+  }
+  else{
+    fs.readFile("404.html", "utf-8", (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        res.end(data);
+      });
+  }
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
 });
 
 server.listen(port, () => {
